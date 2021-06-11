3c78b2baf4b7bc1d662ca8883dbefeb97db4160b
Shepel Pavel <pavel@shepel.net>
2021-06-11 18:01:28 +0300

    style: styled workflow

--- res/_source/js/script.js
+++ res/_source/js/script.js
@@ -4,9 +4,18 @@ const {
     ipcRenderer
 } = require('electron')
 const path = require('path')
-const fs = require('fs');
+const fs = require('fs')
 const $ = require('./../_assets/module/jquery/jquery.min.js')
 const Vue = require('./../_assets/module/vue/vue.min.js')
+
+function openPage(page) {
+    if (!$('.page[data-page="' + page + '"]').hasClass('active')) {
+        $('.js-menu-item').removeClass('active')
+        $('.page').removeClass('active')
+        $('.js-menu-item[data-page="' + page + '"]').addClass('active')
+        $('.page[data-page="' + page + '"]').addClass('active')
+    }
+}
 
 let terminal = new Vue({
     el: '#terminal',
@@ -25,10 +34,26 @@ ipcRenderer.on('system-res', (event, resp) => {
     })
     $('#terminal').stop().animate({
         scrollTop: $('#terminal')[0].scrollHeight
-    }, 800);
-});
+    }, 800)
+})
+
+openPage('apache')
 
 $('.js-button').on('click', function () {
     let _exec = $(this).attr('data-exec')
     ipcRenderer.send(_exec)
-});
+})
+
+$('.js-menu-button').on('click', function () {
+    var _menu_active = $('#menu').hasClass('open')
+    if (_menu_active) {
+        $('#menu').removeClass('open')
+    } else {
+        $('#menu').addClass('open')
+    }
+})
+
+$('.js-menu-item').on('click', function () {
+    var _page = $(this).attr('data-page')
+    openPage(_page)
+})
